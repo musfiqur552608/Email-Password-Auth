cc8ae2c8633720bbdc942084ba18c27c2774910d
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-08-14 02:05:15 +0600

    make a simple form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { getAuth } from "firebase/auth";
 import './App.css';
+import app from "./firebase/firebase.init";
+
+const auth = getAuth(app);
+
+const handleRegister=(event)=>{
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+}
+
+const handleEmailChange = event =>{
+
+}
+
+const handlePasswordChange = event =>{
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleRegister}>
+        <input onBlur={handleEmailChange} type="email" name="email" id="" placeholder="Your Email"/>
+        <br />
+        <input onBlur={handlePasswordChange} type="password" name="password" id="" placeholder="Your Password"/>
+        <br />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
